9e9111d966fcdfadab85fe95bb92fe1da3b8f69a
Uladzislau Leshka <vladik45812@gmail.com>
2022-03-16 14:08:43 +0300

    fix path

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,20 @@
 
 const pictureInnerContainer = document.querySelector('.picture-inner__container'); 
-let imgArray = [`../museum/assets/img/gallery/galery1.jpg`,
-`../museum/assets/img/gallery/galery2.jpg`,
-`../museum/assets/img/gallery/galery3.jpg`,
- `../museum/assets/img/gallery/galery4.jpg`,
- `../museum/assets/img/gallery/galery5.jpg`,
-`../museum/assets/img/gallery/galery6.jpg`,
-`../museum/assets/img/gallery/galery7.jpg`,
-`../museum/assets/img/gallery/galery8.jpg`,
-`../museum/assets/img/gallery/galery9.jpg`,
-`../museum/assets/img/gallery/galery10.jpg`,
-`../museum/assets/img/gallery/galery11.jpg`,
-`../museum/assets/img/gallery/galery12.jpg`,
-`../museum/assets/img/gallery/galery13.jpg`,
-`../museum/assets/img/gallery/galery14.jpg`,
-`../museum/assets/img/gallery/galery15.jpg`];
+let imgArray = [`./assets/img/gallery/galery1.jpg`,
+`./assets/img/gallery/galery2.jpg`,
+`./assets/img/gallery/galery3.jpg`,
+ `./assets/img/gallery/galery4.jpg`,
+ `./assets/img/gallery/galery5.jpg`,
+`./assets/img/gallery/galery6.jpg`,
+`./assets/img/gallery/galery7.jpg`,
+`./assets/img/gallery/galery8.jpg`,
+`./assets/img/gallery/galery9.jpg`,
+`./assets/img/gallery/galery10.jpg`,
+`./assets/img/gallery/galery11.jpg`,
+`./assets/img/gallery/galery12.jpg`,
+`./assets/img/gallery/galery13.jpg`,
+`./assets/img/gallery/galery14.jpg`,
+`./assets/img/gallery/galery15.jpg`];
 function shuffle() {
     imgArray.sort(() => Math.random()- 0.5);
 }
